0b944477791bf35a0c669b16fa2e2a4e2be12c73
Yuwaan <98694815+Yuwaaan@users.noreply.github.com>
2022-03-16 10:54:02 +0000

    Add files via upload

---
+++ my-app/build/precache-manifest.aed5ac3992ff72f327cf35a37b472826.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7b9166e5098693a72584",
+    "url": "./static/css/main.b44e3097.chunk.css"
+  },
+  {
+    "revision": "7b9166e5098693a72584",
+    "url": "./static/js/main.0ec0ffe2.chunk.js"
+  },
+  {
+    "revision": "b50b034dfe69fe6971a8",
+    "url": "./static/js/runtime~main.d5eb009e.js"
+  },
+  {
+    "revision": "aed42cc41ee87f753466",
+    "url": "./static/js/2.746963aa.chunk.js"
+  },
+  {
+    "revision": "17cafacefc0671cbaf3e",
+    "url": "./static/js/3.0a738eea.chunk.js"
+  },
+  {
+    "revision": "a87b9a4f73d584dfa1d37ca6b8c510c7",
+    "url": "./index.html"
+  }
+];
